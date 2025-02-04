5af5f9e9159305b023ab5caa8785f57574da2d29
tosimb <tosimb@yandex.ru>
2025-02-04 19:56:18 +0300

    V0.6 сделана апи для получения данных по ашке

---
+++ src/fa/router.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, HTTPException, Depends, Request
+
+router = APIRouter(
+    prefix="/fa",
+    tags=["F_A"]
+)
+def get_db_pool(request: Request):
+    return request.app.state.db_connection
+
+@router.get("/{name_int}")
+async def get_data_fa(name_int: int, db_connection=Depends(get_db_pool)):
+    try:
+        print(name_int)
+        cursor = db_connection.cursor()
+        cursor.execute("SELECT * FROM fa WHERE id = ?", (name_int,))
+        result = cursor.fetchall()
+        print(result[0][1])
+        cursor.close()
+        if not result:
+            raise HTTPException(status_code=404, detail="Данные не найдены.")
+        return result[0][1]
+    except FileNotFoundError:
+        return ("нет такого узла")
+    except Exception as e:
+        error_str = f"Exception: {e}."
+        raise HTTPException(status_code=527, detail={"error_msg": error_str})
