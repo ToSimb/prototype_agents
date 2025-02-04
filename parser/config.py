a9e1a279ef5deb44af155a3f900f60a3495a6c03
tosimb <tosimb@yandex.ru>
2025-02-04 19:28:31 +0300

    V0.5 Сборшик ашки

---
+++ parser/config.py
@@ -0,0 +1,35 @@
+import os
+
+from dotenv import load_dotenv
+
+
+def str_to_bool(value):
+    if value.lower() in ('true', '1', 't', 'y', 'yes'):
+        return True
+    elif value.lower() in ('false', '0', 'f', 'n', 'no'):
+        return False
+    else:
+        raise ValueError(f"Invalid truth value: {value}")
+
+load_dotenv()
+
+# настройка API F-A
+F_A_True = os.environ.get('F_A_True')
+SERVERS_COUNT = os.environ.get('SERVERS_COUNT')
+BOARDS_COUNT = os.environ.get('BOARDS_COUNT')
+F_A_PROTOCOL = os.environ.get('F_A_PROTOCOL')
+F_A_IP = os.environ.get('F_A_IP')
+F_A_PORT = os.environ.get('F_A_PORT')
+F_A_PATH = os.environ.get('F_A_PATH')
+
+# настройка API F-B
+F_B_True = os.environ.get('F_B_True')
+F_B_PROTOCOL = os.environ.get('F_B_PROTOCOL')
+F_B_IP = os.environ.get('F_B_IP')
+F_B_PORT = os.environ.get('F_B_PORT')
+F_B_PATH = os.environ.get('F_B_PATH')
+
+# общие
+DEBUG = str_to_bool(os.environ.get("DEBUG", "false"))
+TIMER = os.environ.get('TIMER')
+
--- parser/fa.py
+++ parser/fa.py
@@ -1,28 +1,49 @@
+import sqlite3
 import json
+import httpx
+import os
+import sys
+import time
+import signal
 
+from config import (F_A_True,
+                    SERVERS_COUNT,
+                    BOARDS_COUNT,
+                    F_A_PROTOCOL,
+                    F_A_IP,
+                    F_A_PORT,
+                    F_A_PATH,
+                    DEBUG,
+                    TIMER)
 
-def open_file(name_file:str):
-    try:
-        with open(name_file, 'r') as file:
-            data = json.load(file)
-            return data
-    except FileNotFoundError:
-        print("Файл не найден.")
-    except json.JSONDecodeError:
-        print("Ошибка при декодировании JSON.")
-    except Exception as e:
-        print(f"Произошла ошибка: {e}")
+DB_PATH = '../database/my_database.db'
+
+URL = f"{F_A_PROTOCOL}://{F_A_IP}:{F_A_PORT}/{F_A_PATH}"
+if DEBUG:
+    URL = f'http://localhost:8080/freon/22'
+print(URL)
+
+if not os.path.exists(DB_PATH):
+    print(f"Ошибка: база данных {DB_PATH} не найдена.")
+    sys.exit(1)
+conn = sqlite3.connect(DB_PATH)
+print("успешное соединение с БД")
+
+def signal_handler(sig, frame):
+    print("Принят сигнал завершения работы. Закрытие соединения...")
+    conn.close()
+    sys.exit(0)
+
+signal.signal(signal.SIGTERM, signal_handler)
+signal.signal(signal.SIGINT, signal_handler)
 
 def create_id(x:int, y:int):
     return int(x*100+y)
 
-try:
-    platz, uzel = 16, 38
+def parse_response_data_FA (fa: dict):
     data_dict = {}
     ips = []
     error_ips = []
-
-    fa = open_file("json/22.json")
     for i in fa["rows"]:
         if i.get("name") is not ips:
             ips.append(i.get("name"))
@@ -34,22 +55,67 @@ try:
                     "name": i.get("name"),
                     "taskId": i.get("taskId"),
                     "state": i.get("state"),
-                    "units.P": i_stat.get("units")[0].get("P"),
-                    "units.T": i_stat.get("units")[0].get("T"),
-                    "units.U": i_stat.get("units")[0].get("U"),
-                    "units.I": i_stat.get("units")[0].get("I"),
+                    "unit.P": i_stat.get("units")[0].get("P"),
+                    "unit.T": i_stat.get("units")[0].get("T"),
+                    "unit.U": i_stat.get("units")[0].get("U"),
+                    "unit.I": i_stat.get("units")[0].get("I"),
                 }
                 data_dict[i_coords] = i_data
 
         else:
             error_ips.append(i.get("name"))
+        if len(error_ips) > 0:
+            print("Ошибка в получении JSON")
+    return data_dict
 
-    sorted_keys = sorted(data_dict.keys())
-    for i in sorted_keys:
-        print(i, data_dict[i])
+def get_data_from_api(url):
+    try:
+        response = httpx.get(url)
+        response.raise_for_status()
+        return response.json()
 
-    print(len(ips))
-    print(len(error_ips))
+    except Exception as e:
+        print(f"Ошибка при обращении к API: {e}")
+        return None
 
-except:
-    print("Ошибка")
+def update_data(data_dict):
+    cursor = conn.cursor()
+    data_to_update = [(json.dumps(value), key) for key, value in data_dict.items()]
+    cursor.executemany("UPDATE fa SET data = ? WHERE id = ?;", data_to_update)
+    print(f"Обновлено {cursor.rowcount} записей.")
+    conn.commit()
+    cursor.close()
+
+try:
+    while True:
+        data_dict = {}
+        id_count = 0
+        start_time = time.time()
+        fa = get_data_from_api(URL)
+        get_time = time.time()
+        if fa is not None:
+            print("Есть связь")
+            data_dict = parse_response_data_FA(fa)
+            id_count = len(data_dict)
+        parser_time = time.time()
+        for i in range(1, int(SERVERS_COUNT) + 1):
+            for j in range(1, int(BOARDS_COUNT) + 1):
+                key = create_id(i, j)
+                if key not in data_dict:
+                    data_dict[key] = {}
+        add_time = time.time()
+        update_data(data_dict)
+        update_time = time.time()
+        print("______________" + "\n"
+              + "Получение данных:          " + str(get_time - start_time) + "\n"
+              + "Форматирование данных:     " + str(parser_time - get_time) + "\n"
+              + "Количество полученных id:  " + str(id_count) + "/" + str(len(data_dict)) + "\n"
+              + "Добавление id:             " + str(add_time-parser_time) + "\n"
+              + "Обновление БД:             " + str(update_time-add_time))
+        # print(data_dict)
+        time.sleep(int(TIMER))
+
+
+
+except Exception as e:
+    print(f"Ошибка: {e}")
